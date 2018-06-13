81fc307501da30585acd16eb4f38104c6486398c
Emily Vong <emily.vong25@gmail.com>
2018-06-13 21:07:11 +0000

    finished all challenges

--- 2-0/script.js
+++ 2-0/script.js
@@ -18,36 +18,27 @@ function readyFunction(){
 		},
 		{
 			text: "If you're seeing this as a button, congratulations!",
-			href: 'http://twitter.com'
+			href: 'https://twitter.com/'
 		}
 	];
 	for (var i = 0; i<data.length; i++) {
-		//if (data.text) { //what should this be compared to?
-		var btn = document.createElement('BUTTON');
-		var a = document.createElement('A');
-		var btnText = document.createTextNode(data[i].text);
-		btn.appendChild(btnText);
-		document.getElementById('news').appendChild(btn);
-		//var newDiv = document.getElementById("news");
-		//var btnContent = document.createTextNode("<p><button type='button' class='btn btn-default' datahref='"+data[i].href+"><span class='glyphicon glyphicon-star'></span> "+data[i].text+"</button></p>");
-		//newDiv = newDiv + "<p><button type='button' class='btn btn-default' datahref='"+data.href+"><span class='glyphicon glyphicon-star'></span> "+data.text+"</button></p>";
-		//newDiv.appendChild(btnContent);
-		
-	
+		if (data[i].text) { 
+		var newDiv = document.getElementById("news");
+		newDiv.innerHTML += "<p><button type='button' class='btn btn-light' datahref='"+data[i].href+"''><i class='fas fa-star'></i> "+data[i].text+"</button></p>";
+		//can only concatenate to text using innerHTML^^
+
+		}
+	}
+
 	
 
 	var btnsArray = document.getElementsByTagName("button");
 	for(var j = 0; j<btnsArray.length;j++){
-		var btns = btnsArray[j];
-		var a = document.createElement('A');
-		btns.appendChild(a);
-		//btns.href = "https://getbootstrap.com/";
-		//var a = document.createElement('A');
-		//a.setAttribute() = ('href', data[i].href);
-		//btnsArray[j].addEventListener("click",function(event){
-			//if (!target.datahref) { //what should this be compared to?
-				//document.location = target.datahref;
-		//});
-	}
+
+		btnsArray[j].addEventListener("click",function(event){
+			if (event.target.attributes[2].value) { 
+				document.location = event.target.attributes[2].value;
+			}
+		});
 	}
 }
--- 3-0/script.js
+++ 3-0/script.js
@@ -1,23 +1,22 @@
 function readyFunction(){
 
 	var items = document.getElementsByClassName("list-group-item");
-	
 	for(var i = 0; i<items.length;i++) {
 		items[i].addEventListener("click",function(event) {
 
 			for(var j = 0; j<event.target.childNodes.length;j++) {
-				if(event.target.childNodes.tagName=='SPAN') {
-					var num = parseInt(event.target.childNodes[j].innerHTML, 10);
+				
+				if(event.target.childNodes[j].tagName=='SPAN') { //childNodes is an array. must specify index
+					var num = parseInt(event.target.childNodes[j].innerHTML);
 					num--;
-				} //close 1st if statement
 					if (num <= 0) {
+						var elem = event.target;
+						elem.parentNode.removeChild(elem);
 						num = '';
-					} //close 2nd if statement
-					else if(event.target.childNodes[j].innerHTML!='') {
-						event.target.childNodes[j].innerHTML = num;	
-				} //close 3rd if statement
-			
-		} //close 2nd for loop
-	});//close listener function
-} //close 1st for loop
-} //close readyFunction
+					} 
+					if(event.target.childNodes[j].innerHTML!='') event.target.childNodes[j].innerHTML = num;
+				}
+			}
+		});
+	}
+}
